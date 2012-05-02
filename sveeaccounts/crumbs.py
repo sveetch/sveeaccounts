972d32f1589e852b5535966849bf2ae94968bd04
David THENON <sveetch@gmail.com>
2012-05-02 23:15:10 +0200

    Add french translation dictionniary for i18n

--- sveeaccounts/crumbs.py
+++ sveeaccounts/crumbs.py
@@ -8,7 +8,7 @@ from django.utils.translation import ugettext_lazy
 site.update({
     'registration_activation_complete': ugettext_lazy('Registration activation success'),
     'registration_activate': ugettext_lazy('Registration activation fail'),
-    'registration_register': ugettext_lazy('Register a new account'),
+    'registration_register': ugettext_lazy('Register'),
     'registration_complete': ugettext_lazy('Registration complete'),
     'registration_disallowed': ugettext_lazy('Registration disallowed'),
     'auth_login': ugettext_lazy('Login'),
